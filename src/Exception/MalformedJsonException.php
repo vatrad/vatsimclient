24307efcc9501517e3f032082b094cb6db6daa1a
Mike <mike@vatradar.net>
2022-06-12 17:41:31 -0400

    Exceptions

---
+++ src/Exception/MalformedJsonException.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of a VATRadar package.
+ *
+ * Copyright (c) 2022 VATRadar <dev@vatradar.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Vatradar\Vatsimclient\Exception;
+
+class MalformedJsonException extends \Exception
+{
+}
