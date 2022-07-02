13fff275f811dcd34b173d9cdda7a6b3a6635546
Mike <mike@vatradar.net>
2022-07-01 20:33:02 -0400

    extra test files

---
+++ tests/ClientTest.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file is part of a VATRadar package.
+ *
+ * Copyright (c) 2022 VATRadar <dev@vatradar.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+use Vatradar\Vatsimclient\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+/*
+    public function testGetOption()
+    {
+
+    }
+
+    public function testSetOption()
+    {
+
+    }
+
+    public function testBootstrap()
+    {
+
+    }
+
+    public function testRetrieve()
+    {
+
+    }
+
+    public function testHasOption()
+    {
+
+    }
+
+    public function testSetOptions()
+    {
+
+    }
+*/
+}
---
+++ tests/DataFilterTest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of a VATRadar package.
+ *
+ * Copyright (c) 2022 VATRadar <dev@vatradar.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+use Vatradar\Vatsimclient\DataFilter;
+use PHPUnit\Framework\TestCase;
+
+class DataFilterTest extends TestCase
+{
+
+}
